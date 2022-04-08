4a474f455f04f820ba88fd70c1e2ac2cb939425b
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-08 05:56:30 -0300

    two test

---
+++ spec/lib/hero_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'hero'
+
+describe Hero do
+  it 'has a sword' do
+    hero = Hero.new
+    expect(hero.weapon).to eq('sword')
+  end
+
+  it 'has more than 1000 HP points' do
+    hero = Hero.new
+    expect(hero.life).to be >= 1000
+
+  end
+
+end
