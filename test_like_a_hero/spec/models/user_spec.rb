6671b051451ed60f6d6f85dc7e96729ed4f7c454
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-10 09:30:05 -0300

    utilizando gem ffaker no teste

--- test_like_a_hero/spec/models/user_spec.rb
+++ test_like_a_hero/spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid if the level in not between 1 and 99" do
+    nickname = FFaker::Name.first_name
+    kind = %i[knight wizard].sample
+    level = FFaker::Random.rand(100..9999)
+    user = User.new(nickname: nickname, kind: kind, level: level)
+
+    expect(user).to_not be_valid
+  end
+
+  it "returns the correct hero title" do
+    nickname = FFaker::Name.first_name
+    kind = %i[knight wizard].sample
+    level = FFaker::Random.rand(1..99)
+
+    user = User.create(nickname: nickname, kind: kind, level: level)
+    expect(user.title).to eq("#{kind} #{nickname} ##{level}")
+  end
 end
