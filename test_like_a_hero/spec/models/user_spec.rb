b44492e968e8e815b39ccde62f8dfb44fdfb2284
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-04-10 20:59:18 -0300

    usando a gem factorbot, refatorando o models no teste de level

--- test_like_a_hero/spec/models/user_spec.rb
+++ test_like_a_hero/spec/models/user_spec.rb
@@ -2,11 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is invalid if the level in not between 1 and 99" do
-    nickname = FFaker::Name.first_name
-    kind = %i[knight wizard].sample
-    level = FFaker::Random.rand(100..9999)
-    user = User.new(nickname: nickname, kind: kind, level: level)
-
+    user = build(:user, level: FFaker::Random.rand(100..9999))
     expect(user).to_not be_valid
   end
 
@@ -15,7 +11,7 @@ RSpec.describe User, type: :model do
     kind = %i[knight wizard].sample
     level = FFaker::Random.rand(1..99)
 
-    user = User.create(nickname: nickname, kind: kind, level: level)
+    user = create(nickname: nickname, kind: kind, level: level)
     expect(user.title).to eq("#{kind} #{nickname} ##{level}")
   end
 end
